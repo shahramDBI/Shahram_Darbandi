4e171e4c6aaa58b24cc63332e93791039de46622
shahramDBI <shahramdarbandi79@gmail.com>
2023-03-04 17:42:17 +0330

    end

---
+++ script.js
@@ -0,0 +1,15 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+ 
+$(document).ready(function(){
+    $('.progress-value > span').each(function(){
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 1500,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
